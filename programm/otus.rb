fb88d04c421370d9896af21e16e2f4480af96c1d
povarintsev-av <difkust@mail.ru>
2025-03-18 23:50:38 +1000

    101

--- programm/otus.rb
+++ programm/otus.rb
@@ -1,36 +1,32 @@
 require 'yaml'
 require 'securerandom'
 
+PLATFORM = 'Otus'
+LESSON_NAME = 'Lesson'
+MODULE_NAME = 'Module'
+
+Signal.trap('INT') { puts "\nStopping..."; exit }
+
 loop do
+  begin
+    timestamp = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
+    file_name = "otus_lesson_#{timestamp}.yml"
 
-  file_path = "output.#{Time.now.strftime('%H-%M-%S_%Y-%m-%d')}.ymal"
-  lesson = 'Lesson'
-  lesson_id = 11
-  platform = 'Otus'
-  uuid = SecureRandom.uuid
-  m1 = 'Module'
-  m2 = SecureRandom.name
-  m3 = 'UUID'
-
-  data = {
-    platform => {
-
-      lesson => lesson_id,
-      m3 => uuid,
-      m1 => m2
-
+    data = {
+      PLATFORM => {
+        LESSON_NAME => 11,
+        'UUID' => SecureRandom.uuid,
+        MODULE_NAME => SecureRandom.name
+      }
     }
 
-  }
+    File.write(file_name, YAML.dump(data))
+    puts "Created: #{file_name}"
 
-  File.open("#{file_path}", 'w') do |file|
-    YAML.dump(data, file)
+    sleep 10
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+    sleep 5
+    retry
   end
-
-  system("cat #{file_path}")
-  system("rm #{file_path}")
-  sleep 10
-
-rescue StandardError => e
-  puts "ERROR: #{e.message}"
 end
